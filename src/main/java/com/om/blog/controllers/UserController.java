a05b160235de6865bc6aa4cbfa2ab981dd2a8222
omsolanki271 <omsolanki271@gmail.com>
2026-06-28 16:42:41 +0530

    feat : Add UserController

---
+++ src/main/java/com/om/blog/controllers/UserController.java
@@ -0,0 +1,14 @@
+package com.om.blog.controllers;
+
+import com.om.blog.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController
+{
+    @Autowired
+    private UserService userService;
+}
