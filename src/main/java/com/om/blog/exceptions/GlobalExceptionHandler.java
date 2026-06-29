921d0fe87045ba4693637b08ab05d37b8affd6ec
omsolanki271 <omsolanki271@gmail.com>
2026-06-30 01:40:30 +0530

    Fix GlobalExceptionHandler class inheritance

--- src/main/java/com/om/blog/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/om/blog/exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,19 @@
 package com.om.blog.exceptions;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import com.om.blog.payloads.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
-public class GlobalExceptionHandler extends RuntimeException {
+@RestControllerAdvice
+public class GlobalExceptionHandler{
 
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex)
+    {
+        String msg = ex.getMessage();
+        ApiResponse response = new ApiResponse(msg,false);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
 }
