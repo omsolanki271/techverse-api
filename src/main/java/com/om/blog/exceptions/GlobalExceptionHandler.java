176969730f791f10fad7a8a017512d2bf125f97f
omsolanki271 <omsolanki271@gmail.com>
2026-06-30 01:23:00 +0530

    Create Excetionfile is GlobalExceptionHandler with @ControllerAdvice (handle all controller)

---
+++ src/main/java/com/om/blog/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.om.blog.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends RuntimeException {
+
+}
