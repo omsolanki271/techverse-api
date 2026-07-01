bf4b8429abb85a863736f895de457fbd66a6fa11
omsolanki271 <omsolanki271@gmail.com>
2026-07-02 01:45:13 +0530

    Handle bean validation exceptions globally

--- src/main/java/com/om/blog/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/om/blog/exceptions/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.om.blog.exceptions;
 import com.om.blog.payloads.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -28,5 +29,19 @@ public class GlobalExceptionHandler{
                 ex.getName());
         ApiResponse response = new ApiResponse(msg, false);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse> methodArgumentNotValidException(MethodArgumentNotValidException ex)
+    {
+        String msg = "Validation failed";
+
+        if (ex.getBindingResult().getFieldError() != null) {
+            msg = ex.getBindingResult()
+                    .getFieldError()
+                    .getDefaultMessage();
+        }
+        ApiResponse response = new ApiResponse(msg , false);
+        return  new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
     }
 }
