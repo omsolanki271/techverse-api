0900dd7e8aeb236b275ffbae66b2cadde7af5046
omsolanki271 <omsolanki271@gmail.com>
2026-07-19 18:19:40 +0530

    feat: add create and update endpoints for category

--- src/main/java/com/om/blog/controllers/CategoryController.java
+++ src/main/java/com/om/blog/controllers/CategoryController.java
@@ -1,9 +1,12 @@
 package com.om.blog.controllers;
 
+import com.om.blog.payloads.CategoryDto;
 import com.om.blog.services.CategoryService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/category/")
@@ -12,5 +15,19 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    //create category api
+    @PostMapping("/")
+    public ResponseEntity<CategoryDto> createCategory(@Valid @RequestBody CategoryDto categoryDto)
+    {
+        CategoryDto createdCategory = categoryService.createCategory(categoryDto);
+        return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
+    }
 
+
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<CategoryDto> updateCategory(@Valid @RequestBody CategoryDto categoryDto , @PathVariable Integer categoryId)
+    {
+        CategoryDto updateCategory = categoryService.updateCategory(categoryDto, categoryId);
+        return  new ResponseEntity<>(updateCategory,HttpStatus.OK);
+    }
 }
