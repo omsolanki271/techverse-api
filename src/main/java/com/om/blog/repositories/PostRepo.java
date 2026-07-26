0f3df3e7aeab3f340d18e046f09f6a6740d5d723
omsolanki271 <omsolanki271@gmail.com>
2026-07-26 15:07:19 +0530

    feat: add custom repository methods for post filtering

---
+++ src/main/java/com/om/blog/repositories/PostRepo.java
@@ -0,0 +1,15 @@
+package com.om.blog.repositories;
+
+import com.om.blog.entities.Category;
+import com.om.blog.entities.Post;
+import com.om.blog.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostRepo extends JpaRepository<Post , Integer> {
+
+    List<Post> findByUser(User user);
+    List<Post> findByCategory(Category category);
+
+}
