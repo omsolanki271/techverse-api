7796b7d4417094266e6b3cbde42e4571c7887f9b
omsolanki271 <omsolanki271@gmail.com>
2026-07-26 11:52:02 +0530

    feat: add post entity with basic fields

---
+++ src/main/java/com/om/blog/entities/Post.java
@@ -0,0 +1,31 @@
+package com.om.blog.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer postId;
+
+    @Column(name = "post_title", length = 100 , nullable = false)
+    private String title;
+
+    @Column(length = 10000, nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String imageName;
+
+    @Column(nullable = false)
+    private LocalDateTime addedDate;
+
+/*    private Category category;
+    private User user;*/
+}
