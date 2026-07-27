b8c993fd01529f1e161cc4526f2072e1f8b0b6a0
omsolanki271 <omsolanki271@gmail.com>
2026-07-28 00:33:19 +0530

    feat: add validation for post DTO fields

--- src/main/java/com/om/blog/entities/Post.java
+++ src/main/java/com/om/blog/entities/Post.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CurrentTimestamp;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Entity
 @Table(name = "posts")
--- src/main/java/com/om/blog/payloads/PostDto.java
+++ src/main/java/com/om/blog/payloads/PostDto.java
@@ -1,8 +1,8 @@
 package com.om.blog.payloads;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.om.blog.entities.Category;
-import com.om.blog.entities.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -24,8 +24,15 @@ import java.time.LocalDateTime;
 public class PostDto {
 
     private Integer postId;
+
+    @NotBlank(message = "Post title is required")
+    @Size(min = 5, max = 100, message = "Title must be between 5 and 100 characters")
     private String title;
+
+    @NotBlank(message = "Post content is required")
+    @Size(min = 20, max = 10000, message = "Content must be between 20 and 10000 characters")
     private String content;
+
     private String imageName;
     private LocalDateTime addedDate;
     private CategoryDto category;
