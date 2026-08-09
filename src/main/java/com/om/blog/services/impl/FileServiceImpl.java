ef505424b8178aadf1c96abdd78b771795a12146
omsolanki271 <omsolanki271@gmail.com>
2026-08-09 14:48:13 +0530

    feat: implement image upload service

---
+++ src/main/java/com/om/blog/services/impl/FileServiceImpl.java
@@ -0,0 +1,62 @@
+package com.om.blog.services.impl;
+
+import com.om.blog.services.FileService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class FileServiceImpl implements FileService {
+
+    @Override
+    public String uploadImage(String path, MultipartFile file) throws IOException {
+
+        // File name
+        String name = file.getOriginalFilename();
+
+        if (name == null || !name.contains(".")) {
+            throw new IOException("Invalid image file");
+        }
+
+        String extension = name.substring(name.lastIndexOf(".")).toLowerCase();
+
+        if (!extension.equals(".jpg")
+                && !extension.equals(".jpeg")
+                && !extension.equals(".png")
+                && !extension.equals(".gif")
+                && !extension.equals(".webp"))
+        {
+            throw new IOException("Invalid image format. Only JPG, JPEG, PNG, GIF and WEBP are allowed.");
+        }
+
+        String randomId = UUID.randomUUID().toString();
+
+        String fileName  = randomId.concat(extension);
+
+        // Full path
+        String filepath = path + File.separator + fileName ;
+
+        // Create folder if not created
+        File file1 = new File(path);
+
+        if (!file1.exists()) {
+            file1.mkdirs();
+        }
+
+        // File copy
+        Files.copy(file.getInputStream(), Paths.get(filepath));
+
+        return fileName;
+    }
+
+    @Override
+    public InputStream getResource(String path, String fileName) throws FileNotFoundException {
+        return null;
+    }
+
+
+}
