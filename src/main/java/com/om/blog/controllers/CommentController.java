c6dcd26a1599c31cc301aba9a71dcb2ff2629efa
omsolanki271 <omsolanki271@gmail.com>
2026-08-09 22:23:52 +0530

    feat: implement comment API for create and delete

---
+++ src/main/java/com/om/blog/controllers/CommentController.java
@@ -0,0 +1,38 @@
+package com.om.blog.controllers;
+
+import com.om.blog.payloads.ApiResponse;
+import com.om.blog.payloads.CommentDto;
+import com.om.blog.services.CommentService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/comments/")
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @PostMapping("/user/{userId}/post/{postId}")
+    public ResponseEntity<CommentDto> createComment(
+            @Valid @RequestBody CommentDto commentDto ,
+            @PathVariable Integer userId,
+            @PathVariable Integer postId
+    )
+    {
+        CommentDto comment = commentService.createComment(commentDto, postId, userId);
+        return  new ResponseEntity<>(comment, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<ApiResponse> deleteComment(
+            @PathVariable Integer commentId
+    )
+    {
+        commentService.deleteComment(commentId);
+        return  new ResponseEntity<>(new ApiResponse("Comment delete successfully.. ", true),HttpStatus.OK);
+    }
+}
